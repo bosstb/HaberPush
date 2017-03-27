e08a6a8f83bb45f8fefa0e3f8fd73037b75cbf32
bosstb <tangbu@foxmail.com>
2017-03-27 23:12:08 +0800

    2324

--- app.py
+++ app.py
@@ -92,9 +92,8 @@ def wechat():
             content = item.get('content')
             publishedTime = str(item.get('publishedTime')).replace('+0000', '').replace('T', ' ')
             pushInfo = pushInfo + '===============================' + '<a href = "https://compaign.newsgrapeapp.com/news/' + \
-                       sourceId + '">' + title + '[' + str(publishedTime) + ']</a>' + '+++++++' + \
+                       sourceId + '">' + title + ' [' + str(publishedTime) + '] </a>' + '+++++++' + \
             '<a href = "http://haberpush.leanapp.cn/' + sourceId + '?title=' + title + '&content=' + content + '">Push</a>'
-
         replyStr = '<xml><ToUserName>' + FromUserName + '</ToUserName>' + '<FromUserName>' + ToUserName + '</FromUserName>' + '<CreateTime>' + \
         str(time.mktime(datetime.datetime.now().timetuple())).split('.')[0] + '</CreateTime>' + '<MsgType><![CDATA[text]]></MsgType>' + \
         '<Content><![CDATA[' + pushInfo + ']]></Content></xml>'
