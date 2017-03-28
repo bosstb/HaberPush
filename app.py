454c831b32b26427d423a5733365e5c2607622c6
BOSS\tangb <tangbu@foxmail.com>
2017-03-28 21:10:07 +0800

    121

--- app.py
+++ app.py
@@ -95,8 +95,8 @@ def wechat():
                 publishedTime = str(item.get('publishedTime')).replace('+0000', '').replace('T', ' ')
             else:
                 publishedTime = item.get('publishedTime')
-            pushInfo = pushInfo + '===============================' + '<a href = "https://compaign.newsgrapeapp.com/news/' + \
-                       sourceId + '">' + title + '(' + publishedTime + ')</a>' + '+++++++' + \
+            pushInfo = pushInfo + '==' + '<a href = "https://compaign.newsgrapeapp.com/news/' + \
+                       sourceId + '">' + title + '(' + publishedTime + ')</a>' + '++' + \
             '<a href = "http://haberpush.leanapp.cn/' + sourceId + '?title=' + title + '&content=' + content + '">Push</a>'
             if count > 5:
                 break
